4729326929fa01abe3894cd043629a1a6fd40e49
johnlaff <46091881+johnlaff@users.noreply.github.com>
2024-04-21 14:32:37 -0300

    :sparkles: feat: Implementar consulta JPA em ProprietarioController
    Substituída a implementação estática do método listar na classe ProprietarioController por uma consulta JPA. Anteriormente, o método listar retornava uma lista fixa de proprietários. Agora, ele consulta diretamente o banco de dados para obter a lista de proprietários, permitindo que os dados sejam atualizados dinamicamente.

--- src/main/java/com/johnlaff/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/johnlaff/algatransito/api/controller/ProprietarioController.java
@@ -1,30 +1,23 @@
 package com.johnlaff.algatransito.api.controller;
 
 import com.johnlaff.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("João Ferreira");
-        proprietario1.setTelefone("34 99999-1111");
-        proprietario1.setEmail("joaoferreira@gmail.com");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Giordanna Pereira");
-        proprietario2.setTelefone("34 99999-2222");
-        proprietario2.setEmail("giordannapereira@gmail.com");
-
-        return Arrays.asList(proprietario1, proprietario2);
+        return manager.createQuery("from Proprietario", Proprietario.class)
+                .getResultList();
     }
 
 }
