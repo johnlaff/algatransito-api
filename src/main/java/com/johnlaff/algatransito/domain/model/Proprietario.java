c3825a8bb959a5afe036014d8dc1394c1f915b77
johnlaff <46091881+johnlaff@users.noreply.github.com>
2024-04-21 14:29:05 -0300

    :sparkles: feat: Adicionar anotações JPA à classe Proprietario
    Adicionadas anotações JPA à classe Proprietario para mapeamento da entidade e suas propriedades. Isso inclui a definição da estratégia de geração de ID, restrições de nulidade e comprimento para os campos, e a inclusão do ID na geração do hashcode e equals da entidade.

--- src/main/java/com/johnlaff/algatransito/domain/model/Proprietario.java
+++ src/main/java/com/johnlaff/algatransito/domain/model/Proprietario.java
@@ -1,15 +1,30 @@
 package com.johnlaff.algatransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "proprietarios")
 public class Proprietario {
 
-    private Long id;
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "proprietarioid", nullable = false)
+    private Integer id;
+
+    @Column(name = "nome", nullable = false, length = 60)
     private String nome;
+
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name = "telefone", nullable = false, length = 20)
     private String telefone;
 
 }
