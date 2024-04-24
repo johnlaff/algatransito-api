4af34a2c9f1f58e50cad33ca5bf99ed43d49531d
johnlaff <46091881+johnlaff@users.noreply.github.com>
2024-04-24 20:21:00 -0300

    :sparkles: feat: Implementar busca de proprietários por nome
    Adicionado o método findByNomeContaining à interface ProprietarioRepository. Este método retorna uma lista de proprietários cujo nome contém a string fornecida como parâmetro. Isso permite uma busca mais flexível e eficiente por proprietários pelo nome, sem a necessidade de uma correspondência exata. A implementação deste método é gerenciada pelo Spring Data JPA, eliminando a necessidade de consultas SQL manuais.

--- src/main/java/com/johnlaff/algatransito/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/johnlaff/algatransito/domain/repository/ProprietarioRepository.java
@@ -4,6 +4,9 @@ import com.johnlaff.algatransito.domain.model.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Integer> {
+    List<Proprietario> findByNomeContaining(String nome);
 }
