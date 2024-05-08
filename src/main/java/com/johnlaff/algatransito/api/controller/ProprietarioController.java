37f96b4d956e025691ec33ffaa99827ac9489e59
johnlaff <46091881+johnlaff@users.noreply.github.com>
2024-05-08 00:46:44 -0300

    :sparkles: feat: Criar o endpoint de inclusão de recurso
    Adicionado um novo endpoint no ProprietarioController para adicionar um novo proprietário. O método adicionar() foi implementado, que recebe um objeto Proprietario como corpo da requisição e retorna o proprietário salvo com um status 201 (Created). Isso permite a criação de novos proprietários através da API.

--- src/main/java/com/johnlaff/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/johnlaff/algatransito/api/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package com.johnlaff.algatransito.api.controller;
 import com.johnlaff.algatransito.domain.model.Proprietario;
 import com.johnlaff.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +27,12 @@ public class ProprietarioController {
        return proprietarioRepository.findById(proprietarioId)
                .map(ResponseEntity::ok)
                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 
 }
