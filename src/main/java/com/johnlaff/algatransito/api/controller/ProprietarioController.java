c664c882aa4d0d5383c1ea8037866ca08f16b02d
johnlaff <46091881+johnlaff@users.noreply.github.com>
2024-05-08 00:30:40 -0300

    :sparkles: feat: Criar o endpoint de busca de recurso
    Adicionado um novo endpoint no ProprietarioController para buscar um proprietário específico através do seu ID. O método buscar() foi implementado, que recebe o ID do proprietário como parâmetro e retorna o proprietário correspondente, se existir, ou um status 404 (Not Found), se o proprietário não for encontrado. Isso permite uma maior flexibilidade na manipulação de proprietários, permitindo operações individuais em um proprietário específico.

--- src/main/java/com/johnlaff/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/johnlaff/algatransito/api/controller/ProprietarioController.java
@@ -3,20 +3,32 @@ package com.johnlaff.algatransito.api.controller;
 import com.johnlaff.algatransito.domain.model.Proprietario;
 import com.johnlaff.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
     private final ProprietarioRepository proprietarioRepository;
 
-    @GetMapping("/proprietarios")
+    @GetMapping
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Integer proprietarioId) {
+
+       return proprietarioRepository.findById(proprietarioId)
+               .map(ResponseEntity::ok)
+               .orElse(ResponseEntity.notFound().build());
     }
 
 }
